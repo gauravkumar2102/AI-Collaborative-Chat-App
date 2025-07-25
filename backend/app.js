6982b29c19d5165d78523e6b9d8827b7478fccc2
Gaurav kumar <anshusingh2122@gmail.com>
2025-07-25 19:36:50 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -21,11 +21,6 @@ app.use(cors({
  app.use(express.json());
  app.use(express.urlencoded({ extended: true })); 
  app.use(cookieParser());
- app.use((req, res, next) => {
-  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-  next();
-});
 
 
  app.use("/user", userRoutes);
