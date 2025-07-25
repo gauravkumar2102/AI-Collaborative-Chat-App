f0190aafccf188eebafa1165e9f71ab4e8016e34
Gaurav kumar <anshusingh2122@gmail.com>
2025-07-26 02:22:52 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const app=express()
 app.use(morgan('dev'));
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://ai-collaborative-chat-app-1.onrender.com',
   credentials: true, 
   allowedHeaders: ['Content-Type', 'Authorization'], 
 }));
