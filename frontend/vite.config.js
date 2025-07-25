a152af0f4908e21e3d8b1ce3cb74cf27dc78ce26
Gaurav kumar <anshusingh2122@gmail.com>
2025-07-25 15:10:40 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,5 +13,7 @@ export default defineConfig({
     'Cross-Origin-Embedder-Policy': 'require-corp',
     'Cross-Origin-Opener-Policy': 'same-origin'
   }
-}
+}, build: {
+    outDir: 'dist' 
+  }
 })
