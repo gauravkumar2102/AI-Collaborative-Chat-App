4cc95c67469503cfaf4e5a0d2ea9aed83c9b7f4b
Gaurav kumar <anshusingh2122@gmail.com>
2025-08-18 10:20:38 +0530

    remove merge conflit

--- frontend/src/config/webcontainer.js
+++ frontend/src/config/webcontainer.js
@@ -3,7 +3,7 @@ import { WebContainer } from '@webcontainer/api';
 let webContainerInstance = null;
 
 export const getWebContainer = async () => {
-<<<<<<< HEAD
+
   if (!webContainerInstance) {
     webContainerInstance = await WebContainer.boot({
       experimental: {
@@ -14,15 +14,5 @@ export const getWebContainer = async () => {
   }
   return webContainerInstance;
 };
-=======
-    if (webContainerInstance === null) {
-        webContainerInstance = await WebContainer.boot(
-            {
-                experimental: {
-                 permittedPorts: [8080], 
-                 },
-            });
-    }
-    return webContainerInstance;
-}
->>>>>>> d3ee68670c11cf509c037cd71b60dd208716b25e
+
+
